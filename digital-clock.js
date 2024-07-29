ddfbbf8c96487e528a31869ca022f651ffb6e318
Ali Haider <aleehaider953@gmail.com>
2024-07-28 21:10:15 -0700

    Digital clock

---
+++ digital-clock.js
@@ -0,0 +1,6 @@
+let hrs = document.getElementById("hrs");
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+let currentTime = new Data();
+console.log(currentTime);
